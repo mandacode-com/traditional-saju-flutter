56985caeafb870dfe7bf4457180dea6b03968637
sauce-Git <dadad1004@naver.com>
2025-02-11 23:30:51 +0900

    feat: add result enum classes

---
+++ packages/saju_api/lib/models/five_elements.dart
@@ -0,0 +1,19 @@
+/// {@template five_elements}
+/// Five elements
+/// {@endtemplate}
+enum FiveElements {
+  /// 목
+  mok,
+
+  /// 화
+  hwa,
+
+  /// 토
+  to,
+
+  /// 금
+  geum,
+
+  /// 수
+  su,
+}
