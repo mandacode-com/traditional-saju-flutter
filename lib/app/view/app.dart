03d6cb90cf2456a13d15a530db282d3959b76c39
sauce-Git <dadad1004@naver.com>
2025-04-07 03:24:43 +0900

    feat: add dotenv and load environment feature

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -2,6 +2,7 @@ import 'package:api/api.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:repository/repository.dart';
@@ -35,19 +36,29 @@ class _AppState extends State<App> {
 
   late final AuthRepository _authRepository;
 
+  static const googleClientEnvKey = 'GOOGLE_OAUTH_CLIENT_ID';
+
   @override
   void initState() {
     super.initState();
     userHiveStorage.init();
+    final googleOauthClientId = const String.fromEnvironment(
+      googleClientEnvKey,
+    ).isNotEmpty
+        ? const String.fromEnvironment(
+            googleClientEnvKey,
+          )
+        : dotenv.env[googleClientEnvKey] ?? '';
 
     final authDio = Dio(
       BaseOptions(
-        baseUrl: 'https://api.example.com',
+        baseUrl: 'https://auth.mandacode.com/api/core',
         connectTimeout: const Duration(seconds: 5),
         receiveTimeout: const Duration(seconds: 5),
       ),
     );
     final googleSignIn = GoogleSignIn(
+      serverClientId: googleOauthClientId,
       scopes: [
         'email',
         'profile',
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:saju_mobile_v1/app/app.dart';
 import 'package:saju_mobile_v1/bootstrap.dart';
 
-void main() {
-  bootstrap(() => const App());
+Future<void> main() async {
+  await dotenv.load(fileName: 'assets/.env');
+  await bootstrap(() => const App());
 }
