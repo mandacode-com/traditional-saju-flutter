d9c23d76e5b05bc68964c277d4b91bda57d03bac
sauce-Git <dadad1004@naver.com>
2025-04-07 13:48:06 +0900

    chore: saju api update
    Saju api return type updated to return data class directly.

--- packages/api/lib/src/saju_api.dart
+++ packages/api/lib/src/saju_api.dart
@@ -3,7 +3,6 @@ import 'package:api/models/daily_saju/daily_saju_response.dart';
 import 'package:api/models/yearly_saju/yearly_saju_request.dart';
 import 'package:api/models/yearly_saju/yearly_saju_response.dart';
 import 'package:api/src/api_client.dart';
-import 'package:dio/dio.dart';
 
 /// [SajuApi] Saju API class
 class SajuApi {
@@ -13,21 +12,28 @@ class SajuApi {
   final ApiClient _apiClient;
 
   /// [dailySaju] method
-  Future<Response<DailySajuResponse>> dailySaju(
-      DailySajuRequest request) async {
-    return _apiClient.post(
+  Future<DailySajuResponse> dailySaju(DailySajuRequest request) async {
+    final response = await _apiClient.post<Map<String, dynamic>>(
       '/saju/daily',
       data: request.toJson(),
     );
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load daily saju');
+    }
+    return DailySajuResponse.fromJson(response.data!);
   }
 
   /// [yearlySaju] method
-  Future<Response<YearlySajuResponse>> yearlySaju(
+  Future<YearlySajuResponse> yearlySaju(
     YearlySajuRequest request,
   ) async {
-    return _apiClient.post(
+    final response = await _apiClient.post<Map<String, dynamic>>(
       '/saju/yearly',
       data: request.toJson(),
     );
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load yearly saju');
+    }
+    return YearlySajuResponse.fromJson(response.data!);
   }
 }
