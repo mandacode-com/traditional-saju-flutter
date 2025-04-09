ad7e52a88b2b7a6d281c3a0ec1b11c39e67e32f3
sauce-Git <dadad1004@naver.com>
2025-04-09 12:45:35 +0900

    feat: add app repository

---
+++ packages/repository/lib/src/app_repository.dart
@@ -0,0 +1,37 @@
+import 'package:models/models.dart';
+import 'package:storage/storage.dart';
+
+/// [AppRepository] AppRepository class
+class AppRepository {
+  /// [AppRepository] constructor
+  AppRepository({
+    required AppStorage appStorage,
+  }) : _appStorage = appStorage;
+
+  final AppStorage _appStorage;
+
+  /// [saveQuestion] Save question
+  Future<void> saveQuestion(String question) async {
+    await _appStorage.saveQuestion(question);
+  }
+
+  /// [getQuestion] Get question
+  Future<String?> getQuestion() async {
+    return _appStorage.getQuestion();
+  }
+
+  /// [deleteQuestion] Delete question
+  Future<void> deleteQuestion() async {
+    await _appStorage.deleteQuestion();
+  }
+
+  /// [setTargetRoute] Set target route
+  Future<void> setTargetRoute(TargetRoute route) async {
+    await _appStorage.setTargetRoute(route);
+  }
+
+  /// [getTargetRoute] Get target route
+  Future<TargetRoute?> getTargetRoute() async {
+    return _appStorage.getTargetRoute();
+  }
+}
