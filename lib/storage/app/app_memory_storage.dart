1ef1e4f569afcee37bdf076f0aadde497ef834c5
sauce-Git <dadad1004@naver.com>
2025-04-10 17:07:45 +0900

    chore: delete unused app data

--- lib/storage/app/app_memory_storage.dart
+++ lib/storage/app/app_memory_storage.dart
@@ -2,31 +2,15 @@ import 'package:models/models.dart';
 import 'package:storage/storage.dart';
 
 class AppMemoryStorage implements AppStorage {
-  String? _qeustion;
   AppRoutes? _targetRoute;
 
   @override
-  Future<void> deleteQuestion() async {
-    _qeustion = null;
-  }
-
-  @override
-  Future<String?> getQuestion() async {
-    return _qeustion;
-  }
-
-  @override
-  Future<void> saveQuestion(String question) async {
-    _qeustion = question;
-  }
-
-  @override
-  Future<void> setTargetRoute(AppRoutes route) async {
+  void setTargetRoute(AppRoutes route) {
     _targetRoute = route;
   }
 
   @override
-  Future<AppRoutes?> getTargetRoute() async {
+  AppRoutes? getTargetRoute() {
     return _targetRoute;
   }
 }
