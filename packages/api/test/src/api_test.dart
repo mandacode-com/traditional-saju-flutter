b1c6ed8cfe1c5bd1929d974bbfe7d525b822d810
sauce-Git <dadad1004@naver.com>
2025-04-18 18:00:38 +0900

    chore: remove testing temporary

--- packages/api/test/src/api_test.dart
+++ packages/api/test/src/api_test.dart
@@ -1,11 +1,6 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:api/api.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Api', () {
-    test('can be instantiated', () {
-      expect(Api(), isNotNull);
-    });
-  });
+  group('Api', () {});
 }
--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:saju_mobile_v1/app/app.dart';
-import 'package:saju_mobile_v1/counter/counter.dart';
 
 void main() {
-  group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
-    });
-  });
+  group('App', () {});
 }
