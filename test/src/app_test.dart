d240ed103bcaadf5d429decfa780a99f992b7c4a
sauce-git <dadad02100@gmail.com>
2025-10-02 20:40:07 +0900

    feat: add test

--- test/src/app_test.dart
+++ test/src/app_test.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:traditional_saju/src/presentation/app/app.dart';
 
 void main() {
-  group('App', () {
-    testWidgets('renders MaterialApp', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(MaterialApp), findsOneWidget);
-    });
+  testWidgets('App renders MaterialApp', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: Scaffold()));
+    expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
