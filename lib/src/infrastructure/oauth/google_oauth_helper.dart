4977084e3c48460c408ff96df72c40bc4569ecc1
sauce-git <dadad02100@gmail.com>
2025-10-12 19:10:55 +0900

    refactor: update google oauth helper to use client id and client server id

--- lib/src/infrastructure/oauth/google_oauth_helper.dart
+++ lib/src/infrastructure/oauth/google_oauth_helper.dart
@@ -3,15 +3,19 @@ import 'package:google_sign_in/google_sign_in.dart';
 /// Helper class for Google OAuth authentication
 class GoogleOAuthHelper {
   GoogleOAuthHelper({
-    GoogleSignIn? googleSignIn,
-  }) : _googleSignIn = googleSignIn ?? GoogleSignIn.instance;
-
-  final GoogleSignIn _googleSignIn;
+    required String clientId,
+    required String serverClientId,
+  }) {
+    GoogleSignIn.instance.initialize(
+      clientId: clientId,
+      serverClientId: serverClientId,
+    );
+  }
 
   /// Sign in with Google and return the access token
   Future<String?> signIn() async {
     try {
-      final account = await _googleSignIn.authenticate();
+      final account = await GoogleSignIn.instance.authenticate();
 
       final authentication = account.authentication;
       return authentication.idToken;
@@ -22,6 +26,7 @@ class GoogleOAuthHelper {
 
   /// Sign out from Google
   Future<void> signOut() async {
-    await _googleSignIn.signOut();
+    // await _googleSignIn.signOut();
+    await GoogleSignIn.instance.signOut();
   }
 }
